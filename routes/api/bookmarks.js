cafbfa13075dc7fba47ea43c947b02cce956af6a
Danny Wheeler <wheeler@Dannys-MBP.lan>
2024-02-25 11:26:33 -0800

    separate json controller in routes

--- routes/api/bookmarks.js
+++ routes/api/bookmarks.js
@@ -4,14 +4,14 @@ const userController = require('../../controllers/api/users')
 const bookmarkController = require('../../controllers/api/bookmarks')
 
 // index
-router.get('/', bookmarkController.index)
+router.get('/', bookmarkController.index, bookmarkController.jsonBookmarks)
 // delete
-router.delete('/:id', userController.auth, bookmarkController.destroy)
+router.delete('/:id', userController.auth, bookmarkController.destroy, bookmarkController.jsonBookmark)
 // update
-router.put('/:id', userController.auth, bookmarkController.update)
+router.put('/:id', userController.auth, bookmarkController.update, bookmarkController.jsonBookmark)
 // create
-router.post('/', userController.auth, bookmarkController.create)
+router.post('/', userController.auth, bookmarkController.create, bookmarkController.jsonBookmark)
 // show
-router.get('/:id', userController.show)
+router.get('/:id', userController.show, bookmarkController.jsonBookmark)
 
 module.exports = router
