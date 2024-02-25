2251c4e9eac76f017c2abd65640614364ae5ca39
Danny Wheeler <wheeler@Dannys-MBP.lan>
2024-02-25 10:15:09 -0800

    user model

---
+++ models/user.js
@@ -0,0 +1,46 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const passwordValidator = require('password-validator');
+
+const passwordSchema = new passwordValidator()
+passwordSchema
+.is().min(8)                                    // Minimum length 8
+.is().max(100)                                  // Maximum length 100
+.has().uppercase()                              // Must have uppercase letters
+.has().lowercase()                              // Must have lowercase letters
+.has().digits(2)                                // Must have at least 2 digits
+.has().not().spaces()                           // Should not have spaces
+.is().not().oneOf(['Passw0rd', 'Password123']) // Blacklist these values
+
+const userSchema = new mongoose.Schema({
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true},
+    email: {type: String, required: true},
+    password: {type: String, required: true},
+    bookmarks: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Bookmark' }]
+})
+
+
+
+userSchema.pre('save', async function(next) {
+    if (this.isModified('password')) {
+        // validate password
+        const passVal = passwordSchema.validate(this.password, { details: true })
+        if (passVal !== true) {
+            throw new Error(passVal)
+        }
+        this.password = await bcrypt.hash(this.password, 8) // encrypt      
+    }
+    next()
+  })
+
+  userSchema.methods.generateAuthToken = async function() {
+    const token = jwt.sign({ _id: this._id }, process.env.SECRET)
+    return token
+  }
+
+  const User = mongoose.model('User', userSchema)
+
+module.exports = User
